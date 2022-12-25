bce2cd43aa4c88e94e44af8eaf920b9974f12cea
Milan <milanpaulson8@gmail.com>
2022-12-25 12:04:49 +0530

    openAI project

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const dotenv = require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+//enable body parser
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.use('/openai', require('./routes/openairoutes'));
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
